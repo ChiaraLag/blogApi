4b44c1262f1017b8e7e82de490421845217057da
Chiara <chiaralagana.219@gmail.com>
2020-10-16 15:30:50 +0200

    blog

---
+++ app/models/Post.js
@@ -0,0 +1,10 @@
+function Post(author, title, subtitle, body, public, featured, created_date) {
+    this.author = author;
+    this.title = title;
+    this.subtitle = subtitle;
+    this.body = body;
+    this.public = public;
+    this.featured = featured;
+    this.created_date = created_date;
+    this.tag = [];
+}
